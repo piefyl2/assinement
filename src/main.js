44f8c97c60d77949d0d98756cf4b4a8db15751d4
Pierre <piefyl@gmail.com>
2023-08-08 23:12:13 +0200

    Add carrousel

--- src/main.js
+++ src/main.js
@@ -11,16 +11,45 @@ function addCss(file){
     )
 }
 
-function run(){
-    let productDetail = document.getElementsByClassName('product-details-info')[0]
-    let newNode = document.createElement("div");
-    newNode.innerHTML = "<div class='product-recommendation-title'></div>";
-    newNode.className="product-recommendation"
-    productDetail.parentNode.insertBefore(newNode, productDetail.nextSibling);
+function createArrow(){
+    let arrow = document.createElement("div");
+    arrow.className="arrow"
+    arrow.innerHTML = '<div class="center">'+arrow+'</div>'
 }
 
-console.log('1')
+function createProduct(name){
+    let product = document.createElement("div");
+    product.className="product-recommended"
+    product.innerHTML = '<div class="center">'+name+'</div>'
+}
+
+function createCarrousel(nbProductToDisplay){
+    let carrousel = document.createElement("div");
+    carrousel.className="carrousel"
+
+    carrousel.parentNode.append(createArrow('&lt;'));
+    for (let product = 0; product < nbProductToDisplay; product++) {
+        carrousel.parentNode.append(createproduct('Product '+product));
+    }
+    carrousel.parentNode.append(createArrow('&gt;'));
+    <div class="arrow"></div>
+}
+
+function createRecommendation(){
+    let newNode = document.createElement("div");
+    newNode.innerHTML = "<div class='product-recommendation-title'>You may also like:</div>";
+    newNode.className="product-recommendation"
+
+    let carrousel = document.createElement("div");
+    newNode.append(createCarrousel(3));
+    
+}
+
+console.log('V2')
 
 const baseURL = 'https://raw.githubusercontent.com/piefyl2/assinement/dev/src/'
 addCss(baseURL+'inject.css')
-run()
+
+
+let productDetail = document.getElementsByClassName('product-details-info')[0]
+productDetail.parentNode.insertBefore(createRecommendation(), productDetail.nextSibling);
