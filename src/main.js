fe5482b1ca96b4fe1c4ddaf6097070ca88026b98
Pierre <piefyl@gmail.com>
2023-08-09 00:10:28 +0200

    Add first populate

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ function injectCss(file){
       }
     )
 }
-
+// integrate html avoiding CORB
 function injectHTML(file, div){
     fetch(file, {cache: "no-store"})
     .then((response) => response.text())
@@ -20,13 +20,43 @@ function injectHTML(file, div){
     )
 }
 
+class Product {
+    constructor(img, title, price) {
+        this.img = img;
+        this.title = title;
+        this.price = price;
+    }
+}
+
 console.log('V4')
 
 const baseURL = 'https://raw.githubusercontent.com/piefyl2/assinement/dev/src/'
+
+// Add default CSS
 injectCss(baseURL+'inject.css')
 
+// Add carroussel
 let productDetail = document.getElementsByClassName('product-details-info')[0]
-let recommendation = document.createElement("div");
+let recommendation = document.createElement("div")
 injectHTML(baseURL+'inject.html', recommendation)
+productDetail.parentNode.insertBefore(recommendation, productDetail.nextSibling)
 
-productDetail.parentNode.insertBefore(recommendation, productDetail.nextSibling);
+let currentDisplay = 0
+
+// Init model
+let products = []
+for (let product = 0; product < 6; product++) {
+    products[product] = new Product('https://demostore.x-cart.com/images/product/clo_'+product+'.jpg','Product '+product, product+'.00 €')
+}
+
+// First update
+function updateProducts(products, firstDisplay){
+    for (let i = 0; i < 3; i++) {
+        currentUpdate = firstDisplay + i
+        let product = document.getElementById('product-recommended-'+id)
+        product.getElementsByTagName('img')[0].src=products[currentUpdate].title
+        product.getElementById('title').src=products[currentUpdate].price
+    }
+}
+
+updateProducts(products, currentDisplay)
