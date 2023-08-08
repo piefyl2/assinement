fdfed0b1583aa43d9ef8e45ca8813ebc1e88322d
Pierre <piefyl@gmail.com>
2023-08-08 22:24:22 +0200

    Add title

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+// integrate css avoiding CORB
+function addCss(file){
+    fetch(file, {cache: "no-store"})
+    .then((response) => response.text())
+    .then((text) => {
+      const style = document.createElement('style');
+      style.textContent = text;
+      document.head.append(style);
+      eval(style)
+      }
+    )
+}
+
 function run(){
     let productDetail = document.getElementsByClassName('product-details-info')[0]
     let newNode = document.createElement("div");
-    newNode.innerHTML = "It's a Javascript book";
+    newNode.innerHTML = "<div class='product-recommendation-title'></div>";
     newNode.className="product-recommendation"
     productDetail.parentNode.insertBefore(newNode, productDetail.nextSibling);
 }
@@ -9,19 +22,5 @@ function run(){
 console.log('1')
 
 const baseURL = 'https://raw.githubusercontent.com/piefyl2/assinement/dev/src/'
-// integrate css avoiding CORB
-fetch(baseURL+'inject.css', {cache: "no-store"})
-  .then((response) => response.text())
-  .then((text) => {
-    const style = document.createElement('style');
-    style.textContent = text;
-    document.head.append(style);
-    eval(style)
-    }
-  )
-
-
-document.getElementsByTagName('head')[0].insertAdjacentHTML(
-    'beforeend',
-    '<link rel="stylesheet" href="'+ />');
+addCss(baseURL+'inject.css')
 run()
