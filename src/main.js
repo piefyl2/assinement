6de9d24d6581b9a984b7619eff1e53ece9f1b911
Pierre <piefyl@gmail.com>
2023-08-08 23:40:17 +0200

    Use template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // integrate css avoiding CORB
-function addCss(file){
+function injectCss(file){
     fetch(file, {cache: "no-store"})
     .then((response) => response.text())
     .then((text) => {
@@ -11,44 +11,22 @@ function addCss(file){
     )
 }
 
-function createArrow(){
-    let arrow = document.createElement("div");
-    arrow.className="arrow"
-    arrow.innerHTML = '<div class="center">'+arrow+'</div>'
+function injectHTML(file, div){
+    fetch(file, {cache: "no-store"})
+    .then((response) => response.text())
+    .then((text) => {
+        div.innerHTML = text
+      }
+    )
 }
 
-function createProduct(name){
-    let product = document.createElement("div");
-    product.className="product-recommended"
-    product.innerHTML = '<div class="center">'+name+'</div>'
-}
-
-function createCarrousel(nbProductToDisplay){
-    let carrousel = document.createElement("div");
-    carrousel.className="carrousel"
-
-    carrousel.parentNode.append(createArrow('&lt;'));
-    for (let product = 0; product < nbProductToDisplay; product++) {
-        carrousel.parentNode.append(createproduct('Product '+product));
-    }
-    carrousel.parentNode.append(createArrow('&gt;'));
-}
-
-function createRecommendation(){
-    let newNode = document.createElement("div");
-    newNode.innerHTML = "<div class='product-recommendation-title'>You may also like:</div>";
-    newNode.className="product-recommendation"
-
-    let carrousel = document.createElement("div");
-    newNode.append(createCarrousel(3));
-    
-}
-
-console.log('V3')
+console.log('V4')
 
 const baseURL = 'https://raw.githubusercontent.com/piefyl2/assinement/dev/src/'
-addCss(baseURL+'inject.css')
-
+injectCss(baseURL+'inject.css')
 
 let productDetail = document.getElementsByClassName('product-details-info')[0]
-productDetail.parentNode.insertBefore(createRecommendation(), productDetail.nextSibling);
+let recommendation = document.createElement("div");
+injectHTML(baseURL+'inject.html', recommendation)
+
+productDetail.parentNode.insertBefore(recommendation, productDetail.nextSibling);
