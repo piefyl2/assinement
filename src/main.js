e21cca94c154fa556bd43e5a87eeaf146324d4da
Pierre <piefyl@gmail.com>
2023-08-09 00:54:24 +0200

    Add support of keyboards

--- src/main.js
+++ src/main.js
@@ -22,42 +22,8 @@ function injectHTML(file, div){
     )
 }
 
-class Product {
-    constructor(img, title, price) {
-        this.img = img;
-        this.title = title;
-        this.price = price;
-    }
-}
-
-console.log('V4')
-
-const baseURL = 'https://raw.githubusercontent.com/piefyl2/assinement/dev/src/'
-
-// Add default CSS
-injectCss(baseURL+'inject.css')
-
-// Add carroussel
-let productDetail = document.getElementsByClassName('product-details-info')[0]
-let recommendation = document.createElement("div")
-injectHTML(baseURL+'inject.html', recommendation)
-productDetail.parentNode.insertBefore(recommendation, productDetail.nextSibling)
-
-let currentDisplay = 0
-
-// Init model
-let products = [
-new Product('https://demostore.x-cart.com/images/product/clo_1.jpg','Product 1', '1.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_3.jpg','Product 2', '2.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_4.jpg','Product 3', '3.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_5.jpg','Product 4', '4.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_6.jpg','Product 5', '5.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_9.jpg','Product 6', '6.00 €')
-]
-
-// First update
+// Update carousel display
 function updateProducts(direction){
-    console.log(currentDisplay)
     currentDisplay = (currentDisplay+direction)%products.length
     while(currentDisplay<0){
         currentDisplay += (products.length)
@@ -72,3 +38,45 @@ function updateProducts(direction){
         product.querySelector('#price').textContent=products[currentUpdate].price
     }
 }
+
+class Product {
+    constructor(img, title, price) {
+        this.img = img;
+        this.title = title;
+        this.price = price;
+    }
+}
+
+const baseURL = 'https://raw.githubusercontent.com/piefyl2/assinement/dev/src/'
+
+// Add default CSS
+injectCss(baseURL+'inject.css')
+
+// Add carroussel
+let productDetail = document.getElementsByClassName('product-details-info')[0]
+let recommendation = document.createElement("div")
+injectHTML(baseURL+'inject.html', recommendation)
+productDetail.parentNode.insertBefore(recommendation, productDetail.nextSibling)
+
+// add action on keyboards
+document.addEventListener("keydown", function(event) {
+    if (event.key == "ArrowLeft"){
+        updateProducts(1)
+    } else if (event.key == "ArrowRight"){
+        updateProducts(-1)
+    }
+ });
+
+
+
+let currentDisplay = 0
+
+// Init model
+let products = [
+new Product('https://demostore.x-cart.com/images/product/clo_1.jpg','Product 1', '1.00 €'),
+new Product('https://demostore.x-cart.com/images/product/clo_3.jpg','Product 2', '2.00 €'),
+new Product('https://demostore.x-cart.com/images/product/clo_4.jpg','Product 3', '3.00 €'),
+new Product('https://demostore.x-cart.com/images/product/clo_5.jpg','Product 4', '4.00 €'),
+new Product('https://demostore.x-cart.com/images/product/clo_6.jpg','Product 5', '5.00 €'),
+new Product('https://demostore.x-cart.com/images/product/clo_9.jpg','Product 6', '6.00 €')
+]
