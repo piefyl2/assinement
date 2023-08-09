c73f0bab1f5d3ef47c6ca18294eeff27e5a14bab
Pierre <piefyl@gmail.com>
2023-08-09 02:01:13 +0200

    Add random producs

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ function updateProducts(direction){
     for (let i = 0; i < 3; i++) {
         currentUpdate = (currentDisplay + i) % products.length
         let product = document.getElementById('product-recommended-'+i)
-        product.href = products[currentUpdate].img
+        product.href = products[currentUpdate].url
         product.querySelector('img').src=products[currentUpdate].img
         product.querySelector('#title').textContent=products[currentUpdate].title
         product.querySelector('#price').textContent=products[currentUpdate].price
@@ -51,7 +51,8 @@ function previous(){
 }
 
 class Product {
-    constructor(img, title, price) {
+    constructor(url, img, title, price) {
+        this.url = url;
         this.img = img;
         this.title = title;
         this.price = price;
@@ -81,11 +82,24 @@ document.addEventListener("keydown", function(event) {
 let currentDisplay = 0
 
 // Init model
+
+fetch('https://fakestoreapi.com/products?limit=6')
+            .then(res=>res.json())
+            .then(json=> {
+                console.log(json)
+                for (let index = 0; index < json.length; index++) {
+                    let fakeproduct = json[index]
+                    console.log(fakeproduct)
+                    products[index]=new Product('https://demostore.x-cart.com/',fakeproduct.image,fakeproduct.title, fakeproduct.price)
+                }
+                updateProducts(0)
+        })
+
 let products = [
-new Product('https://demostore.x-cart.com/images/product/clo_1.jpg','Product 1', '1.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_3.jpg','Product 2', '2.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_4.jpg','Product 3', '3.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_5.jpg','Product 4', '4.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_6.jpg','Product 5', '5.00 €'),
-new Product('https://demostore.x-cart.com/images/product/clo_9.jpg','Product 6', '6.00 €')
+new Product('https://demostore.x-cart.com/','https://demostore.x-cart.com/images/product/clo_1.jpg','Product 1', '1.00 €'),
+new Product('https://demostore.x-cart.com/','https://demostore.x-cart.com/images/product/clo_3.jpg','Product 2', '2.00 €'),
+new Product('https://demostore.x-cart.com/','https://demostore.x-cart.com/images/product/clo_4.jpg','Product 3', '3.00 €'),
+new Product('https://demostore.x-cart.com/','https://demostore.x-cart.com/images/product/clo_5.jpg','Product 4', '4.00 €'),
+new Product('https://demostore.x-cart.com/','https://demostore.x-cart.com/images/product/clo_6.jpg','Product 5', '5.00 €'),
+new Product('https://demostore.x-cart.com/','https://demostore.x-cart.com/images/product/clo_9.jpg','Product 6', '6.00 €')
 ]
